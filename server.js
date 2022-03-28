84648b8ca307755b37178d0d356b7ff7f840ba30
Melina C <melange@Melinas-MacBook-Air.local>
2022-03-28 11:02:14 -0400

    Connected seed database can see json in browser

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+const pokemon = require('./models/pokemon')
 
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App');
 });
+
+app.get('/', (req, res) => {
+    res.send(pokemon)
+});
+
+
+
 app.listen(3000, () => {
     console.log('Servers are a go!');
 });
