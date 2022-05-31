5f50d7356c43f8cada7388a1afa4723738cc1a09
Jeanette <melange@Melinas-Air.hsd1.ma.comcast.net>
2022-05-31 15:21:18 -0400

    pokemon schema created

--- models/pokemon.js
+++ models/pokemon.js
@@ -1,12 +1,25 @@
-const pokemon = [
-    { name: "bulbasaur", img: "http://img.pokemondb.net/artwork/bulbasaur" },
-    { name: "ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur" },
-    { name: "venusaur", img: "http://img.pokemondb.net/artwork/venusaur" },
-    { name: "charmander", img: "http://img.pokemondb.net/artwork/charmander" },
-    { name: "charizard", img: "http://img.pokemondb.net/artwork/charizard" },
-    { name: "squirtle", img: "http://img.pokemondb.net/artwork/squirtle" },
-    { name: "wartortle", img: "http://img.pokemondb.net/artwork/wartortle" }
-];
+const mongoose = require("./connection");
+
+const { Schema, model } = mongoose;
+
+const pokemonSchema = new Schema({
+    name: String,
+    image: String,
+})
+
+const Pokemon = model("Pokemon", pokemonSchema);
+
+module.exports = Pokemon;
+
+// const pokemon = [
+//     { name: "bulbasaur", img: "http://img.pokemondb.net/artwork/bulbasaur" },
+//     { name: "ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur" },
+//     { name: "venusaur", img: "http://img.pokemondb.net/artwork/venusaur" },
+//     { name: "charmander", img: "http://img.pokemondb.net/artwork/charmander" },
+//     { name: "charizard", img: "http://img.pokemondb.net/artwork/charizard" },
+//     { name: "squirtle", img: "http://img.pokemondb.net/artwork/squirtle" },
+//     { name: "wartortle", img: "http://img.pokemondb.net/artwork/wartortle" }
+// ];
 
 
 
